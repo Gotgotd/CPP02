a242997d8a53f4d2ddac49aff1a7d9edb74901d7
Gautier Daigneau <gdaignea@c1r3p6.42perpignan.fr>
2024-09-09 17:29:05 +0200

    added more tests pour ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gdaignea <gdaignea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 17:54:11 by gdaignea          #+#    #+#             */
-/*   Updated: 2024/08/23 11:45:47 by gdaignea         ###   ########.fr       */
+/*   Updated: 2024/09/09 14:53:22 by gdaignea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,16 @@ int main( void )
 {
 	Fixed   a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-	//Fixed const b( Fixed( 5.05f ) / Fixed( 2 ) );
+	//Fixed const b( Fixed( 5.05f ) / Fixed( 2 ) ); //resultat attendu : 2.52344
+	//Fixed const b( Fixed( 5.05f ) + Fixed( 2 ) ); //resultat attendu : 7.05078 
+	//Fixed const b( Fixed( 5.05f ) - Fixed( 2 ) ); //resultat attendu : 3.05078
 
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
+	//std::cout << --a << std::endl;
 	std::cout << a << std::endl;
 	std::cout << a++ << std::endl;
+	//std::cout << a-- << std::endl;
 	std::cout << a << std::endl;
 	
 	std::cout << b << std::endl;
@@ -31,3 +35,15 @@ int main( void )
 
 	return 0;
 }
+
+/* output pour --a et a-- : 
+
+0
+-0.00390625
+-0.00390625
+-0.00390625
+-0.0078125
+10.1016
+10.1016
+
+ */
